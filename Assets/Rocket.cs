42128604ef665a184311b659de487142c48d1b65
ToniRajamaki <zingoo16@gmail.com>
2020-09-16 19:46:50 +0300

    Added Rocket script and implemented shell for inputs

---
+++ Assets/Rocket.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+        
+        
+    }
+    
+    private void ProcessInput()
+    {
+       
+        if(Input.GetKey(KeyCode.Space) || 
+           Input.GetKey(KeyCode.W))
+        {
+            print("SPace");
+        }
+        if(Input.GetKey(KeyCode.A))
+        {
+            print("LEFT");
+        }
+        
+        if(Input.GetKey(KeyCode.D))
+        {
+        
+            print("RIGHT");
+        
+        }
+
+    }
+}
