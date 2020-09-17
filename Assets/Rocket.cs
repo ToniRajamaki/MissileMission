0f86129fcbdf7cee01d46a6ab67a82996486a5ed
ToniRajamaki <zingoo16@gmail.com>
2020-09-17 12:00:56 +0300

    Added basic collision framework

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -25,6 +25,19 @@ public class Rocket : MonoBehaviour
         Rotate();
     }
 
+    void OnCollisionEnter(Collision other) 
+    {
+        switch (other.gameObject.tag)
+        {
+            case "Friendly":
+                print("OK");
+                break;
+            default:
+                print("DEAD"); // todo kill the player
+                break;
+        }
+    }
+
     private void Thrust()
     {
         if (Input.GetKey(KeyCode.Space) ||
